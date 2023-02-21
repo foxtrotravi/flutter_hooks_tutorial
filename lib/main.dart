a184246576777ac2470d54bfb88c425bbe586df2
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-02-21 22:20:52 +0530

    Simple counter app using hooks

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends HookWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final counter = useState<int>(0);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            counter.value.toString(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => counter.value++,
+          child: const Icon(Icons.add),
         ),
       ),
     );
